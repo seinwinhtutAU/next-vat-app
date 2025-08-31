619d42e14ef00b6b3cb966e5d7a3776f7abbd711
seinwinhtutAU <u6611040@au.edu>
2025-08-31 22:34:52 +0700

    Update next.config.ts

--- next-vat-app/next.config.ts
+++ next-vat-app/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  /** @type {import('next').NextConfig} */ 
-  const nextConfig = {
-    basePath: "/app/app1", 
-  }
+  basePath: "/app/app1",   
 };
 
 export default nextConfig;
